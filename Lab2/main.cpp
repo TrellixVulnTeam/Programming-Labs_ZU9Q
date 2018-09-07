ac303340f748d34acd4c86703f99612919021d83
ShkalikovOleh <Shkalikov.Oleh@outlook.com>
2018-09-07 19:49:03 +0300

    lab2 task1 complete

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -2,5 +2,15 @@
 
 int main() {
 	
+	double a, b;
+	std::cout << "Enter a ";
+	std::cin >> a;
+	std::cout << "Enter b ";
+	std::cin >> b;
+	if (a != b)
+		std::cout << "min " << ((a > b) ? b : a) << std::endl;
+	else
+		std::cout << "You must enter different number";
+
 	return 0;
  }
