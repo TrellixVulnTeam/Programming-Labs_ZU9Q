dbcba4c2103c49642db837441c1e3cabc6aebfd0
ShkalikovOleh <Shkalikov.Oleh@outlook.com>
2018-09-11 19:20:46 +0300

    Lab4 create

---
+++ Lab4/Task.h
@@ -0,0 +1,11 @@
+#pragma once
+
+extern double min_base_task(double*);
+
+extern double prod_base_task(double*);
+
+extern double sum_medium_task(double*);
+
+extern double max_medium_task(double*);
+
+extern double* grad_hard_task(double*);
