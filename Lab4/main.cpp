d8275cb10081abf359ddfeefca2763692bc6ba40
ShkalikovOleh <Shkalikov.Oleh@outlook.com>
2018-09-13 19:30:27 +0300

    Lab 4 Header file create

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -1,7 +1,33 @@
 #include "Task.h"
 #include <iostream>
 
-int main() {
+using namespace std;
+
+int main() {	
+	cout << "Base task" << endl << endl;
+
+	int m, n;
+	cout << "Enter a row count";
+	cin >> m;
+	cout << "Enter a column count";
+	cin >> n;
+	double **matrix = new double*[m];
+	for (int i = 0; i < n; i++)
+		matrix[i] = new double[n];
+
+	cout << "Min:" << min_base_task(matrix) << endl;
+	cout << "Prod:" << prod_base_task(matrix) << endl;
+
+
+	cout << "Medium task" << endl << endl;
+
+	int k;
+	cout << "Enter n from n*n matrix size";
+	cin >> k;
+
+
+	cout << "Max:" <<max_medium_task() << endl;
+	cout << "Sum:" << sum_medium_task() << endl;
 
 	return 0;
 }
