d51bdb9a74af6261b5c8c80f4bf922b218deb94c
ShkalikovOleh <Shkalikov.Oleh@outlook.com>
2018-09-15 16:12:05 +0300

    Lab5 hard_task complete

--- Lab5/Task.h
+++ Lab5/Task.h
@@ -1,9 +1,9 @@
 #pragma once
-#include <unordered_set>
+#include <unordered_map>
 #include <string>
 
 bool sum_base_task(const char*);
 
 bool dump8_medium_task(const char*);
 
-std::unordered_set<std::string, int> bigramm_hard_task(const char*);
+std::unordered_map<std::string, int> bigramm_hard_task(const char*);
