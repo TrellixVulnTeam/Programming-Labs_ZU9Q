77e9a04466965895b389dd2481af467058477351
ShkalikovOleh <Shkalikov.Oleh@outlook.com>
2018-09-15 15:44:50 +0300

    Lab5 base_task complete

--- Lab5/base_task.cpp
+++ Lab5/base_task.cpp
@@ -1,5 +1,19 @@
 #include <fstream>
 
-bool sum_base_task(char* path) {
-	return true;
+bool sum_base_task(const char* path) {
+	std::ifstream input(path);
+	if (input) {
+		double a, b;
+		input >> a;
+		input >> b;
+		std::ofstream output("test.out");
+		if (output) {
+			output << a + b;
+		}
+		else
+			return false;
+		return true;
+	}
+	else
+		return false;
 }
--- Lab5/main.cpp
+++ Lab5/main.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main() {
-
+	cout << "Base task" << endl << endl;	
+	if (!sum_base_task("test.in"))
+		cout << "Error in base task" << endl;
+	if (!dump8_medium_task("binary.txt"))
+		cout << "Error in medium task" << endl;
 	return 0;
 }
