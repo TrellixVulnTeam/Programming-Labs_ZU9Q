6b0a983b830dc7303f0d6786e461eb1695479f71
ShkalikovOleh <Shkalikov.Oleh@outlook.com>
2018-09-17 15:09:54 +0300

    Lab6 base_task complete

--- Lab6/Task.h
+++ Lab6/Task.h
@@ -6,6 +6,6 @@ int size_base_task(std::string);
 
 void printPart_add_task(std::string);
 
-double lab4sum_base_task(std::vector<std::vector<double>>);
+double lab4min_base_task(std::vector<std::vector<double>>);
 
 double lab4prod_base_task(std::vector<std::vector<double>>);
--- Lab6/base_task.cpp
+++ Lab6/base_task.cpp
@@ -1,5 +1,65 @@
 #include <string>
+#include <vector>
+#include <array>
 
 int size_base_task(std::string string) {
 	return string.size();
+}
+
+double lab4min_base_task(std::vector<std::vector<double>> matrix)
+{
+	double min = matrix[0][0];
+	for (auto it = matrix.cbegin(); it != matrix.cend(); it++) {
+		for (auto jt = (*it).cbegin(); jt != (*it).cend(); jt++) {
+			if (min > *jt)				
+				min = *jt;
+		}
+	}
+	return min;
+};
+
+double lab4prod_base_task(std::vector<std::vector<double>> matrix)
+{
+	double prod = 0;
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix[i].size(); j++) {
+			double t = matrix[i][j];
+			if (t > 0) {
+				if (prod != 0)
+					prod *= t;
+				else
+					prod = t;
+			}
+		}
+	}
+	return prod;
+}
+
+template<int m, int n>
+double alt_lab4min_base_task(std::array<std::array<double, n>, m> matrix) {
+	double min = matrix[0][0];
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix[i].size(); j++) {
+			if (min > matrix[i][j])
+				min = matrix[i][j];
+		}
+	}
+	return min;
+}
+
+template<int m, int n>
+double alt_lab4prod_base_task(std::array<std::array<double, n>, m> matrix) {
+	double prod = 0;
+	for (int i = 0; i < matrix.size(); i++) {
+		for (int j = 0; j < matrix[i].size(); j++) {
+			double t = matrix[i][j];
+			if (t > 0) {
+				if (prod != 0)
+					prod *= t;
+				else
+					prod = t;
+			}
+		}
+	}
+	return prod;
 }
