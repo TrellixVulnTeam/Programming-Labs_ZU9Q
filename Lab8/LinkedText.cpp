c12e09af2d643a223919099348e92711c6c5d3c2
ShkalikovOleh <Shkalikov.Oleh@outlook.com>
2018-09-18 20:54:12 +0300

    Lab8 LinkedText definition complete

---
+++ Lab8/LinkedText.cpp
@@ -0,0 +1,45 @@
+#include "LinkedText.h"
+
+
+
+LinkedText::LinkedText()
+{
+}
+
+
+LinkedText::~LinkedText()
+{
+}
+
+void LinkedText::Save(std::string path)
+{
+}
+
+void LinkedText::Load(std::string path)
+{
+}
+
+void LinkedText::AddLine(std::string)
+{
+}
+
+void LinkedText::CopyLine(number, number)
+{
+}
+
+void LinkedText::RemoveLine(number)
+{
+}
+
+void LinkedText::FindLetter(char letter, number &, number &)
+{
+}
+
+number LinkedText::GetLineWithMaxLetterContains(char letter)
+{
+	return number();
+}
+
+void LinkedText::ConsolePrint()
+{
+}
