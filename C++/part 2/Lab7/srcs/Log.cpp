48f2af5fb1072f4c3a2e14323c2f444531a162bc
Oleh Shkalikov <Shkalikov.Oleh@outlook.com>
2021-02-07 11:47:10 +0200

    Remove C++ lab 7 part 2

--- C++/part 2/Lab7/srcs/Log.cpp
+++
@@ -1,8 +0,0 @@
-#include "Log.h"
-
-StreamLog::StreamLog(std::ostream& stream):stream(stream){};  
-
-void StreamLog::Log(std::string info)
-{
-    stream << info << std::endl;
-};
--- C++/part 2/Lab7/tests/IDGeneratorTests.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include "../srcs/IDGenerator.h"
-
-TEST(IDGeneratorTests, GetID)
-{
-    auto id1 = IDGenerator::GetInstance().GetID();
-    auto id2 = IDGenerator::GetInstance().GetID();
-    ASSERT_NE(id1, id2);
-}
